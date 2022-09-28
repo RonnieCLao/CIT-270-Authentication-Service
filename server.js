08fc540c631f4503d4cdcd1ef59e1bd9628ec665
Ronnie Lao <lao21001@byui.edu>
2022-09-28 11:54:59 -0600

    Monday server add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser");
 const {v4 : uuidv4} = require("uuid");
+const { json } = require('body-parser');
 const port = 3000;
 const app = express();
 
@@ -14,4 +15,21 @@ app.get('/', (req,res)=>{
     res.send('Hello World!')
 });
 
+
+app.post("/login", (req,res)=>{
+    const loginEmail = req.body.userName;
+    console.log(JSON.stringify(req.body));
+    console.log("loginEmail", loginEmail);
+    const loginPassword = req.body.password;
+    console.log("loginPassword", loginPassword);
+    res.sendStatus("Who are you");
+
+    if (loginEmail == "test12@gmail.com" && loginPassword == "123456Cc"){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized
+        res.send("Invalid user or password");
+    }
+})
 
