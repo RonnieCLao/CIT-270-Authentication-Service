5b89eba3afaac7ad5d4b5971655646ae1d009a58
Ronnie Lao <lao21001@byui.edu>
2022-09-30 12:55:25 -0600

    Wednesday port 6379

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const {v4 : uuidv4} = require("uuid");
 const { json } = require('body-parser');
 const port = 3000;
 const app = express();
+const {createClient} = require('redis');
+const md5 = require('md5');
+const redisClient = creatClient({
+
+    url:'redis://default@34.132.23.7:6379',
+})
 
 app.use(bodyParser.json());
 
 app.listen(port, async ()=>{
-    console.log('listening on port '+port);
+    await redisClient.connect();
+    console.log('listening on port: ',port);
 })
 
 app.get('/', (req,res)=>{
@@ -19,17 +26,17 @@ app.get('/', (req,res)=>{
 app.post("/login", (req,res)=>{
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
-    console.log("loginEmail", loginEmail);
+    console.log('loginEmail', loginEmail);
     const loginPassword = req.body.password;
-    console.log("loginPassword", loginPassword);
-    res.sendStatus("Who are you");
+    console.log('loginPassword', loginPassword);
+    res.sendStatus('Who are you');
 
-    if (loginEmail == "test12@gmail.com" && loginPassword == "123456Cc"){
+    if (loginEmail == 'test12@gmail.com' && loginPassword == 'p@ssw0rd'){
         const token = uuidv4();
         res.send(token);
     } else{
         res.status(401);//unauthorized
-        res.send("Invalid user or password");
+        res.send('Invalid user or password');
     }
 })
 
